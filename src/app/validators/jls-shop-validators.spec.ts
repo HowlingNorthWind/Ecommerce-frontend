f2e6d445aa912ea06eb60ca239300dc15f2a5fb2
JingzeLiu99 <178373892@qq.com>
2022-08-31 14:56:12 -0400

    add customer validation

---
+++ src/app/validators/jls-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { JLsShopValidators } from './jls-shop-validators';
+
+describe('JLsShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new JLsShopValidators()).toBeTruthy();
+  });
+});
